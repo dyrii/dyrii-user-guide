7f3ce3035752648e52a7b96aed21aa120aae4aa7
dyrii <35407380+dyrii@users.noreply.github.com>
2018-09-15 14:36:40 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,8 @@ import sys, os
 from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
 
+def setup(app):
+    app.add_stylesheet('theme_override.css')
 
 lexers['php'] = PhpLexer(startinline=True, linenos=1)
 lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
